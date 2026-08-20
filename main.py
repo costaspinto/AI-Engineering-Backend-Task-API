e761cc8e5ad706b84c48415e24db387b09cc1f0e
Costas Pinto <costaspinto312@gmail.com>
2026-08-21 00:12:09 +0530

    Stage 4: update task endpoint

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ class TaskCreate(BaseModel):
     done: bool = False
 
 
+class TaskUpdate(BaseModel):
+    title: str
+    done: bool
+
 tasks = [
     {
         "id": 1,
@@ -65,3 +69,16 @@ def create_task(task: TaskCreate):
 
     tasks.append(new_task)
     return new_task
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, task: TaskUpdate):
+    for existing_task in tasks:
+        if existing_task["id"] == task_id:
+            existing_task["title"] = task.title
+            existing_task["done"] = task.done
+            return existing_task
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
